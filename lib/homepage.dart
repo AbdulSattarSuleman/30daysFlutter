20afd1979df21b90707df04320acfba6d78e650c
Abdul Sattar Suleman <43110862+AbdulSattarSuleman@users.noreply.github.com>
2021-08-09 23:31:50 +0500

    product dummy list generate

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:day3/models/catalog.dart';
 import 'package:day3/utils/routes.dart';
 import 'package:day3/widgets/drawer.dart';
+import 'package:day3/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,17 +11,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     //Context =? Element = Location
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Catalog App'),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text('Catalog App'),
+        centerTitle: true,
+      ),
+      drawer: MyDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
-        drawer: MyDrawer(),
-        body: Container(
-          child: Center(
-            child: Text(context.runtimeType.toString()),
-          ),
-        ));
+      ),
+    );
   }
 }
